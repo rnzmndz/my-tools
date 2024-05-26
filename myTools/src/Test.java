2083a0e3a56be175812823063e2a387311700826
Renzo Mendoza <renzomendoza.me@gmail.com>
2024-05-26 21:23:00 +0800

    Save

--- myTools/src/Test.java
+++ myTools/src/Test.java
@@ -3,7 +3,7 @@ import formula.PumpSize;
 public class Test {
 
 	public static void main(String[] args) {
-	var obj = new PumpSize(750, 72, 1);
+	var obj = new PumpSize(500, 100, 1);
 	System.out.println(obj.getMotorSize());
 	obj.setPumpEfficiency(.80);;
 	System.out.println(obj.getMotorSize());
