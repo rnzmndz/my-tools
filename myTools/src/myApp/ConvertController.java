2f1bdbbddba95239de0b375f9076e0eada282672
Renzo Mendoza <renzomendoza.me@gmail.com>
2024-06-10 17:12:14 +0800

    UPDATED 5:11 pm 06/10/24

--- myTools/src/myApp/ConvertController.java
+++ myTools/src/myApp/ConvertController.java
@@ -1,17 +1,32 @@
 package myApp;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 
-public class ConvertController implements BaseController{
+public class ConvertController implements BaseController, Initializable{
+	static List<String> comboBoxContents = new ArrayList<String>();
 	private Main main;
 	
 	@FXML
 	ComboBox<String> comboBoxUnits1;
+	
+	@FXML
+	ComboBox<String> comboBoxUnits2;
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		comboBoxUnits1.getItems().addAll("Initial Item 1", "Initial Item 2", "Initial Item 3");
+		comboBoxUnits2.getItems().addAll("Initial Item 1", "Initial Item 2", "Initial Item 3");
+	}
 	
 	public void setMain(Main main) {
 		this.main = main;
@@ -26,7 +41,17 @@ public class ConvertController implements BaseController{
 	private void switchConversion(ActionEvent event) {
 		main.showScene("ChooseUnits.fxml", "Conversion", chooseUnitsController.class);
 	}
-	public void setComboBoxContent(List<String> content) {
-		comboBoxUnits1.getItems().addAll(content);
+	
+	@FXML
+	public void changeComboBoxContent() {
+		comboBoxUnits1.getItems().clear();
+		comboBoxUnits1.getItems().addAll(ConvertController.comboBoxContents);
+		comboBoxUnits2.getItems().clear();
+		comboBoxUnits2.getItems().addAll(ConvertController.comboBoxContents);
+	}
+	
+	public static void setComboBoxContent(String[] content) {
+		comboBoxContents.clear();
+		Collections.addAll(comboBoxContents, content);
 	}
 }
--- myTools/src/myApp/chooseUnitsController.java
+++ myTools/src/myApp/chooseUnitsController.java
@@ -1,15 +1,10 @@
 package myApp;
 
 import javafx.fxml.FXML;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import javafx.event.ActionEvent;
 
 public class chooseUnitsController implements BaseController{
 	private Main main;
-	private List<String> comboBoxContents = new ArrayList<String>();
 	
 	public void setMain(Main main) {
 		this.main = main;
@@ -23,9 +18,7 @@ public class chooseUnitsController implements BaseController{
 	@FXML
 	private void switchLength(ActionEvent event) {
 		String[] units = {"meter", "kilometer", "centimeter", "millimeter", "micrometer", "nanometer", "inch", "foot", "yard", "miles", "nautical_miles"};
+		ConvertController.setComboBoxContent(units);
 		main.showScene("Convert.fxml", "Length", ConvertController.class);
-		comboBoxContents.clear();
-		Collections.addAll(comboBoxContents, units);
-		ConvertController.set
 	}
 }
